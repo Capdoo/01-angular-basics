b3d24fb96cf178e38b2081be422a9eb518200975
Capdoo <mazher_2014@hotmail.com>
2024-01-16 21:21:20 -0500

    test commit 2

--- 00-typescript-basics/src/ejercicios/01-tipos-basicos.ts
+++ 00-typescript-basics/src/ejercicios/01-tipos-basicos.ts
@@ -9,9 +9,11 @@
 */
 let nombre:string = 'Strider';
 let hp: number | string = 95;
+let hp2: number | string = 96;
 let estaVivo: boolean = true;
 
 hp = 'FULL';
+hp2 = 'FULL';
 
 
 console.log(nombre,hp);
